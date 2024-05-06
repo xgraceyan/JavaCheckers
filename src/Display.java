6ebfc231647b1aa8a1876cbe19a7b93afe312637
Grace Yan <xgraceyan@gmail.com>
2024-05-05 21:59:50 -0700

    gui improvements

--- src/Display.java
+++ src/Display.java
@@ -2,15 +2,19 @@ import jdk.dynalink.beans.StaticClass;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
-public class Display {
+public class Display implements ActionListener {
     private CheckerBoard cb = new CheckerBoard();
-    JFrame f = new JFrame();
+    private JButton[][] buttonList = new JButton[8][8];
+    private ArrayList<JButton> checkerButtons = new ArrayList<>();
+    private JFrame f = new JFrame();
+    private Checker selected;
 
     public Display() {
         f.setLayout(new GridLayout(8, 8));
-        JButton[] buttonList = new JButton[64];
-
 
         for (int i = 0; i < 64; i++) {
 
@@ -21,29 +25,31 @@ public class Display {
             Color dark_brown = new Color(135, 99, 41);
 
             if (cb.isEmpty(x, y)) {
-                buttonList[i] = new JButton();
+                buttonList[x][y] = new JButton();
                 if (i / 8 % 2 == 1) {
-                    if (i % 2 == 0) buttonList[i].setBackground(dark_brown);
-                    else buttonList[i].setBackground(light_brown);
+                    if (i % 2 == 0) buttonList[x][y].setBackground(dark_brown);
+                    else buttonList[x][y].setBackground(light_brown);
                 } else {
-                    if (i % 2 == 1) buttonList[i].setBackground(dark_brown);
-                    else buttonList[i].setBackground(light_brown);
+                    if (i % 2 == 1) buttonList[x][y].setBackground(dark_brown);
+                    else buttonList[x][y].setBackground(light_brown);
                 }
             } else {
                 Checker c = cb.getChecker(x, y);
                 Icon blackIcon = new ImageIcon("resources/black_checker.png");
                 Icon whiteIcon = new ImageIcon("resources/white_checker.png");
-                System.out.println(blackIcon.getIconHeight());
+
                 if (c.getColor().equals("white"))
-                    buttonList[i] = new JButton(whiteIcon);
+                    buttonList[x][y] = new JButton(whiteIcon);
                 else
-                    buttonList[i] = new JButton(blackIcon);
+                    buttonList[x][y] = new JButton(blackIcon);
+                checkerButtons.add(buttonList[x][y]);
             }
 
-
-            buttonList[i].setOpaque(true);
-            buttonList[i].setBorderPainted(false);
-            f.add(buttonList[i]);
+            buttonList[x][y].addActionListener(this);
+            buttonList[x][y].setOpaque(true);
+            buttonList[x][y].setBorderPainted(false);
+            buttonList[x][y].setName((char) x + " " + (char) y);
+            f.add(buttonList[x][y]);
         }
 
 
@@ -55,7 +61,39 @@ public class Display {
         f.setVisible(true);
     }
 
-    public void handleMove(Checker c, int newX, int newY) {
+    public void handleMove(int newX, int newY) {
         // TODO implement this later
+        JButton checkerButton = buttonList[selected.getX()][selected.getY()];
+        JButton newButton = buttonList[newX][newY];
+        System.out.println(checkerButton.getIcon());
+        checkerButton.setBackground(newButton.getBackground());
+        newButton.setIcon(checkerButton.getIcon());
+        checkerButton.setIcon(null);
+        buttonList[selected.getX()][selected.getY()] = newButton;
+        buttonList[newX][newY] = checkerButton;
+        cb.moveChecker(selected, newX, newY);
+        selected = null;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (selected != null) {
+            JButton button = (JButton) e.getSource();
+            int x = button.getName().charAt(0);
+            int y = button.getName().charAt(2);
+            handleMove(x, y);
+            return;
+        }
+
+        for (JButton checkerButton : checkerButtons) {
+            if (checkerButton == e.getSource()) {
+                // select a checker
+                int x = checkerButton.getName().charAt(0);
+                int y = checkerButton.getName().charAt(2);
+                selected = cb.getChecker(x, y);
+                System.out.println(selected.getColor());
+                return;
+            }
+        }
     }
 }
