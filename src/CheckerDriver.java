55dc0b8415c9eddef42ebd8cf7f11790f56e867a
leka-ekambaram <167143535+leka-ekambaram@users.noreply.github.com>
2024-05-15 11:47:50 -0700

    Update CheckerDriver.java
    adding the code for the player and computer to move for each turn.

--- src/CheckerDriver.java
+++ src/CheckerDriver.java
@@ -7,22 +7,32 @@ import java.util.Scanner;
 public class CheckerDriver {
 	public static void main(String[] args) {
 		Display d = new Display(); 
-        	d.showBoard();
+					d.showBoard();
 		 Scanner sc = new Scanner(System.in); 
-	        System.out.println("Enter player name. (Do not enter in name 'computer' for game to function): ");
+					System.out.println("Enter player name. (Do not enter in name 'computer' for game to function): ");
 					String nameS = sc.nextLine();
-	        while(nameS.equals("computer") || nameS.length() < 3)
-	        {
-	            System.out.println("Error. Please do not enter in 'computer' as your name. And make sure your name is at least 3 characters long. Please try again and enter your name. ");
+					while(nameS.equals("computer") || nameS.length() < 3)
+					{
+							System.out.println("Error. Please do not enter in 'computer' as your name. And make sure your name is at least 3 characters long. Please try again and enter your name. ");
 							nameS = sc.nextLine(); 
-	        }
+					}
 		System.out.println("Alright, your name is " + nameS + ". ");
 		System.out.println("Enter Favorite Number: ");
 		int favNum = sc.nextInt();
 		String userName = "Checker Player " + nameS.substring(0,3) + favNum; 
 		System.out.println("Your username that will reference you is " + userName);
-	        Human h = new Human(userName, true); 
-	        CheckerBoard c = new CheckerBoard(); 
-		
+					Human h = new Human(userName, true); 
+					CheckerBoard c = new CheckerBoard(); 
+					Computer comp = new Computer(false);
+					d.setTurn(true);
+					while ((c.getWhiteScore() != 12 || c.getBlackScore() != 12) && d.getTurn()) {
+						d.setTurn(false);
+						System.out.println("It is " + userName + "'s turn. ");
+						if (d.getTurn()) {
+							System.out.println("It is the computer's turn. Wait for the computer to make a move before playing.");
+							//here's where the method for the computer moving would be, i'll work on it soon!
+						}
+						
+					}
 	}
 }
