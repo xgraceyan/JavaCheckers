bb8d388092fbeda004198f13d2b6cd457e431e07
leka-ekambaram <167143535+leka-ekambaram@users.noreply.github.com>
2024-05-15 09:40:50 -0700

    Update CheckerDriver.java
    Fixed code so that scanner takes in name.

--- src/CheckerDriver.java
+++ src/CheckerDriver.java
@@ -9,19 +9,18 @@ public class CheckerDriver {
 		Display d = new Display(); 
         	d.showBoard();
 		 Scanner sc = new Scanner(System.in); 
-	       	String nameS; 
 	        System.out.println("Enter player name. (Do not enter in name 'computer' for game to function): ");
-
-	        while(sc.nextLine().equals("computer"))
+					String nameS = sc.nextLine();
+	        while(nameS.equals("computer"))
 	        {
 	            System.out.println("Error. Please do not enter in 'computer' as your name. Please try again and enter your name. ");
-
+							nameS = sc.nextLine(); 
 	        }
-	        nameS = sc.nextLine(); 
+		System.out.println("Alright, your name is " + nameS + ". ");
 
 	        Human h = new Human(nameS, true); 
 	        CheckerBoard c = new CheckerBoard(); 
 	        h.playGame(c); 
 	        h.startChecker(c);
-}
+	}
 }
