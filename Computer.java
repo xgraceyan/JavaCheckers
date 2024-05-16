6983a128acce51b744ebca2b3b2f1a3211ebf601
leka-ekambaram <167143535+leka-ekambaram@users.noreply.github.com>
2024-05-15 23:38:16 -0700

    Update Computer.java

--- Computer.java
+++ Computer.java
@@ -79,12 +79,12 @@ public class Computer extends Player {
           System.out.println("Indices: " + x + " " + y + " " + indices[2] + " " + indices[3]);
           return indices;
     }
-  
-  /**
-    * Method that returns whether the given indices result in a valid move 
-    *
-    * @return boolean - true if it is a valid move and false otherwise   
-    */ 
+
+    /**
+     * Method that returns whether the given indices result in a valid move 
+     *
+     * @return boolean - true if it is a valid move and false otherwise   
+     */ 
   public boolean isValidMove(CheckerBoard board, int startX, int startY, int endX, int endY) {
 
     // Check if the start and end positions are within the bounds of the board
@@ -107,8 +107,6 @@ public class Computer extends Player {
     if (!board.isEmpty(endX, endY)) {
         return false; 
     }
-
-    //idk guys any other checks I need to add here?
     return true;
   }
 
