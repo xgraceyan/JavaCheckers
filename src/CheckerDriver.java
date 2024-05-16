de6669f9f31bc49762a6d02281307dcc8910ef2b
leka-ekambaram <167143535+leka-ekambaram@users.noreply.github.com>
2024-05-15 23:30:08 -0700

    Update CheckerDriver.java

--- src/CheckerDriver.java
+++ src/CheckerDriver.java
@@ -7,29 +7,24 @@ import java.util.Scanner;
 public class CheckerDriver {
 	public static void main(String[] args) {
 		Display d = new Display(); 
-					d.showBoard();
-		 Scanner sc = new Scanner(System.in); 
-					System.out.println("Enter player name. (Do not enter in name 'computer' for game to function): ");
-					String nameS = sc.nextLine();
-					while(nameS.equals("computer") || nameS.length() < 3)
-					{
-							System.out.println("Error. Please do not enter in 'computer' as your name. And make sure your name is at least 3 characters long. Please try again and enter your name. ");
-							nameS = sc.nextLine(); 
-					}
+		d.showBoard();
+		
+		Scanner sc = new Scanner(System.in); 
+		System.out.println("Enter player name. (Do not enter in name 'computer' for game to function): ");
+		String nameS = sc.nextLine();
+		while(nameS.equals("computer") || nameS.length() < 3)
+		{
+			System.out.println("Error. Please do not enter in 'computer' as your name. And make sure your name is at least 3 characters long. Please try again and enter your name. ");
+			nameS = sc.nextLine(); 
+		}
+
 		System.out.println("Alright, your name is " + nameS + ". ");
 		System.out.println("Enter Favorite Number: ");
 		int favNum = sc.nextInt();
 		String userName = "Checker Player " + nameS.substring(0,3) + favNum; 
 		System.out.println("Your username that will reference you is " + userName);
 					Human h = new Human(userName, true); 
-					CheckerBoard c = new CheckerBoard(); 
-					Computer comp = new Computer(false);
-					while (c.getWhiteScore() != 12 && c.getBlackScore() != 12) {
-						h.playGame(c, d, comp, userName);
-						System.out.print("..");
-						comp.playGame(c, d);
-					}
-						System.out.print("hi");
-						
+					CheckerBoard c = new CheckerBoard();
+					System.out.print("Start the game by selecting a checker piece to move. Next, click the spot you want the checker to move to.");
 	}
 }
