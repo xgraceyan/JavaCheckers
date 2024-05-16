2c69816baa40f0f13369f6a3c7dc3e615130b05e
s2zhang <167555226+s2zhang@users.noreply.github.com>
2024-05-15 17:23:17 -0700

    Add files via upload

--- Computer.java
+++ Computer.java
@@ -2,78 +2,59 @@ import java.util.Random;
 /**
  * Computer class to play checkers based on randomly generated valid moves. The Computer class extends the Player class.
  * @author: Sophia Zhang
- */
+ **/
 
 public class Computer extends Player {
   private Random rand;
-  private final int BOARD_SIZE;
-  private Display d;
   
   /**
    * Constructor that sets Computer class 
    *  
    * @param turn - determines if it is the Computer's turn 
    */
-  public Computer(boolean turn){
-    super("Computer", turn);
-    rand = new Random();
-    BOARD_SIZE = 8;
-    d = new Display();
-  }
+    public Computer(boolean turn){
+        super("Computer", turn);
+        rand = new Random();
+    }
 
+    /**
+     * Selects a random white checker piece from the checkerboard.
+     *
+     * @param board The checkerboard.
+     * @return The selected checker, or null if no valid checker is found.
+    */
+    private Checker selectRandomChecker(CheckerBoard board) {
+        int x = rand.nextInt(8);
+        int y = rand.nextInt(8);
+        Checker c = null;
+
+        while( c == null || !c.getColor().equals("white") || !board.moveChecker(x, y, c) || board.isEmpty(x, y)){
+            x = random.nextInt(8);
+            y = random.nextInt(8);
+            c = board.getChecker(x, y);
+    }
+        return c; 
+}
+    
   /**
    * Method for the Computer to make a move 
    *  
    * @param board -- CheckerBoard 
    */
-  public void makeMove(CheckerBoard board){
-    Checker toMove = board.getChecker(rand.nextInt(BOARD_SIZE), rand.nextInt(BOARD_SIZE));
-    while (board.isEmpty(toMove.getX(), toMove.getY())) {
-      toMove = board.getChecker(rand.nextInt(BOARD_SIZE), rand.nextInt(BOARD_SIZE));
-    }
-        int startX = toMove.getX();
-        int startY = toMove.getY();
-        int endX = rand.nextInt(BOARD_SIZE);
-        int endY = rand.nextInt(BOARD_SIZE);
-        while (!board.isEmpty(toMove.getX(), toMove.getY())) {
-          endX = rand.nextInt(BOARD_SIZE);
-          endY = rand.nextInt(BOARD_SIZE);
-        }
-        if (!isValidMove(board, startX, startY, endX, endY)) {
-          return makeMove(board);
-        }
-        d.handleMove(startX, startY, endX, endY);
-  }
+    public void makeRandomMove(CheckerBoard board){
     
-public boolean isValidMove(CheckerBoard board, int startX, int startY, int endX, int endY) {
-    
-    // Check if the start and end positions are within the bounds of the board
-    if ((startX - 1 != endX || startX + 1 != endX) && startY - 1 != endY) {
-        if ((startX + 2 == endX && board.getChecker(startX + 1, startY + 1).getColor().equals("black")) || (startX - 2 == endX && board.getChecker(startX - 1, startY + 1).getColor().equals("black")) && startY + 2 == endY) {
-          return true;
+        Checker toMove = selectRandomChecker(board);
+        if (toMove != null) {
+          int startX = checker.getX();
+          int startY = checker.getY();
         }
-        else return false;
-    }
-    return true;
+        int newX = rand.nextInt(8);
+        int newY = rand.nextInt(8);
+
+        while(!board.moveChecker(toMove, newX, newY)){
+            newX = rand.nextInt(8);
+            newY = rand.nextInt(8);
+        }
+        moveChecker(toMove, newX, newY);
   }
-
-    // Retrieve the checker at the start position
-    Checker startChecker = board.getChecker(startX, startY);
-
-    // Check if the start position contains a checker and if it belongs to the computer player
-    if (startChecker == null || super.isHuman()) {
-        return false; 
-    }
-
-    // Check if the end position is empty
-    if (!board.isEmpty(endX, endY)) {
-        return false; 
-    }
-
-    //idk guys any other checks I need to add here?
-    return true;
-  }
-  
-  */
-  
 }
