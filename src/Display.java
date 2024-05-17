c453a2a0f1a8bf153d94dfb9276941c194385708
yann5365 <167555245+yann5365@users.noreply.github.com>
2024-05-16 19:26:40 -0700

    Add files via upload

--- src/Display.java
+++ src/Display.java
@@ -7,25 +7,70 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 /**
-* Display class to manage the actual GUI for checkers 
-* Author: Grace Yan 
-**/
+ * Display class to manage the actual GUI for checkers
+ * Author: Grace Yan
+ **/
 public class Display implements ActionListener {
     private CheckerBoard cb = new CheckerBoard();
     private JButton[][] buttonList = new JButton[8][8];
     private ArrayList<JButton> checkerButtons = new ArrayList<>();
     private JFrame f = new JFrame();
+    private JPanel p = new JPanel();
+    private JPanel p2 = new JPanel();
+    private JPanel all = new JPanel();
+    private JLabel welcome;
+    private JTextField prompt;
+    private JTextField prompt2;
+    private JTextField userInput = new JTextField(15);
     private Checker selected;
     private boolean turnOver;
-    private Computer comp; 
+    private Computer comp;
+    private boolean nameEntered;
 
     /**
-     * Constructor for Display class 
-     * Initializes the JFrame and JButtons 2D array (responsible for our checkerboard) 
+     * Constructor for Display class
+     * Initializes the JFrame and JButtons 2D array (responsible for our checkerboard)
      **/
     public Display() {
-        f.setLayout(new GridLayout(8, 8));
         String identity;
+        nameEntered = false;
+        all.setLayout(new BoxLayout(all, BoxLayout.X_AXIS));
+        p.setLayout(new GridLayout(8, 8));
+        p2.setLayout(new BoxLayout(p2, BoxLayout.PAGE_AXIS));
+
+        prompt = new JTextField("Enter player name below.");
+        prompt2 = new JTextField("For game to function, do not enter \"computer\".");
+        prompt.setBounds(50, 50, 200, 100);
+        prompt.setEditable(false);
+        prompt2.setEditable(false);
+
+        JPanel space = new JPanel();
+        space.setBackground(new Color(238, 238, 238));
+        space.setPreferredSize(new Dimension(400, 600));
+
+        welcome = new JLabel("Welcome to Checkers!");
+        welcome.setFont(new Font("calibri", Font.BOLD, 20));
+        userInput.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String input = userInput.getText();
+                String name = "";
+                if (input.length() < 3) {
+                    prompt2.setText("Please make sure your player name is at least three characters long.");
+                } else if (input.equalsIgnoreCase("computer")) {
+                    prompt2.setText("Error. Please do not enter in 'computer' as your name.");
+                } else if (!nameEntered) {
+                    name = input;
+                    prompt.setText("Hello " + input + "! Please enter your favorite number.");
+                    userInput.setText("");
+                    prompt2.setVisible(false);
+                    nameEntered = true;
+                } else {
+                    String username = "HumanPlayer_" + name.substring(0, 3) + Integer.parseInt(input);
+                    prompt.setText("The username that will reference you will be: " + username);
+                    userInput.setText("");
+                }
+            }
+        });
 
         for (int i = 0; i < 64; i++) {
 
@@ -57,40 +102,60 @@ public class Display implements ActionListener {
                     buttonList[x][y] = new JButton(blackIcon);
                 checkerButtons.add(buttonList[x][y]);
             }
-
-
             buttonList[x][y].addActionListener(this);
             buttonList[x][y].setOpaque(true);
             buttonList[x][y].setBorderPainted(false);
             buttonList[x][y].setName((char) x + " " + (char) y);
-            f.add(buttonList[x][y]);
+            buttonList[x][y].setPreferredSize(new Dimension(100, 100));
+            p.add(buttonList[x][y]);
         }
 
         turnOver = false;
         comp = new Computer(false);
-        f.setSize(800, 800);
+        f.setSize(1200, 800);
+        p.setSize(800, 800);
+        p2.setSize(400, 800);
+        p2.add(welcome);
+        p2.add(prompt);
+        p2.add(prompt2);
+        p2.add(userInput);
+        p2.add(space);
+        /* p2.add(welcome, BorderLayout.PAGE_START);
+        p2.add(prompt, BorderLayout.CENTER);
+        p2.add(userInput, BorderLayout.CENTER); */
+        all.add(p);
+        all.add(p2);
+        f.add(all);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
+    public void setPrompt(String str) {
+        prompt.setText(str);
+    }
+
     /**
-     * Method that makes the JFrame visible 
+     * Method that makes the JFrame visible
      **/
     public void showBoard() {
         f.setVisible(true);
     }
 
+    public boolean getTurn() {
+        return turnOver;
+    }
+
     /**
-     * Method that handles the human player's checker move on the display 
+     * Method that handles the human player's checker move on the display
      **/
     public boolean handleMove(Checker c, int oldX, int oldY, int newX, int newY) {
         boolean valid = true;
         if (c != null) {
-        	valid = cb.moveChecker(c, newX, newY);
+            valid = cb.moveChecker(c, newX, newY);
         }
-       // System.out.println(newX + " " + newY);
+        // System.out.println(newX + " " + newY);
         // TODO implement this later
         if (valid) {
-        	//System.out.println(newX + " " + newY);
+            //System.out.println(newX + " " + newY);
             JButton checkerButton = buttonList[oldX][oldY];
             JButton newButton = buttonList[newX][newY];
 
@@ -125,10 +190,10 @@ public class Display implements ActionListener {
     }
 
     /**
-     * Overloaded method that handles the computer's checker move on the display 
+     * Overloaded method that handles the computer's checker move on the display
      **/
     public void handleMoveComp(int formerX, int formerY, int newX, int newY) {
-         handleMove(null, formerX, formerY, newX, newY);
+        handleMove(null, formerX, formerY, newX, newY);
     }
 
     /**
@@ -141,11 +206,10 @@ public class Display implements ActionListener {
             int x = button.getName().charAt(0);
             int y = button.getName().charAt(2);
             if (handleMove(selected, selected.getX(), selected.getY(), x, y)) {
-            	selected = null;
-            	int[] indices = comp.makeRandomMove(cb);
-            	handleMoveComp(indices[0], indices[1], indices[2], indices[3]);
+                selected = null;
+                int[] indices = comp.makeRandomMove(cb);
+                handleMoveComp(indices[0], indices[1], indices[2], indices[3]);
             }
-            
             return;
         }
 
@@ -162,8 +226,8 @@ public class Display implements ActionListener {
                 return;
             }
         }
-        
-        
+
+
     }
 
 }
